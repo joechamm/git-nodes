d2735f4addc3fda26855badd410d37a3fc2f643d
Joseph Cunningham <joe@joechamm.com>
2014-08-19 10:34:03 -0400

    created template to generate all classes

---
+++ 687-git-refdb-node.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2014 Tailored Cloud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+  var nodegit = require('nodegit');
+
+  function refdb_node(n) {
+    RED.nodes.createNode(this,n);
+    this.topic = n.topic;
+    this.git_command = n.git_command;
+    this.git_command_arguments = n.git_command_arguments;
+    this.git_return_type = n.git_return_type;
+    var node = this;
+
+    this.on('input', function (msg) {
+      console.log(msg);
+
+      var newmsg,
+        RefDb_method = nodegit.RefDb[node.git_command],
+        has_callback = false,
+        resolved_arguments = [],
+        cb_string,
+        cb_args;
+
+      newmsg = {
+        "topic":node.topic,
+        "payload":{
+          "return_type":node.git_return_type,
+          "return_value":""
+        }
+      };
+
+      node.git_command_arguments.forEach(function (elem,idx,array) {
+        switch(elem.type) {
+          case "Function":
+            has_callback = true;
+            break;
+          case "CloneOptions":
+            resolved_arguments.push(null);
+            break;
+          default:
+            resolved_arguments.push(msg.payload[elem.name]);
+            break;
+        }
+      });
+
+      if (has_callback) {
+        var cb = ( function (innode,inmsg) { return function (err,ret) {
+            if (err) throw err;
+            inmsg.payload.return_value = ret;
+            innode.send(inmsg);
+          }
+        })(node,newmsg);
+
+        resolved_arguments.push(cb);
+        RefDb_method.apply(RefDb_method,resolved_arguments);
+      } else {
+        newmsg.payload.return_value = RefDb_method.apply(RefDb_method,resolved_arguments);
+        node.send(newmsg);
+      }
+    });
+  }
+
+  RED.nodes.registerType('refdb_node',refdb_node);
+
+  refdb_node.prototype.close = function () {
+    console.log('refdb_node closed');
+  };
+}
